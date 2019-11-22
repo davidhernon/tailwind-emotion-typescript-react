545a3eed871b344ecf6481a21210e8e467313c17
davidhernon <david.hernon1@gmail.com>
2019-11-21 22:48:35 -0500

    pushing latest

---
+++ build/precache-manifest.9b8ec257421e142a882fc8b496b1e922.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66a785540524d07daf529439ec6fa591",
+    "url": "/tailwind-emotion-typescript-react/index.html"
+  },
+  {
+    "revision": "15ece2bc7623faf96f0a",
+    "url": "/tailwind-emotion-typescript-react/static/css/main.ef24b163.chunk.css"
+  },
+  {
+    "revision": "bd323a77b9a4a7727991",
+    "url": "/tailwind-emotion-typescript-react/static/js/2.f80d5485.chunk.js"
+  },
+  {
+    "revision": "15ece2bc7623faf96f0a",
+    "url": "/tailwind-emotion-typescript-react/static/js/main.f71cea39.chunk.js"
+  },
+  {
+    "revision": "e691fb413f6eeae3cd89",
+    "url": "/tailwind-emotion-typescript-react/static/js/runtime-main.c62df17f.js"
+  }
+]);
